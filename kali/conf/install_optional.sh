d824cb98f52f6ad18f184958296004bf0a112e99
jochemste <jochemstevense@hotmail.com>
2021-12-09 16:16:14 +0100

    fixed mistake in  install optional script

--- kali/conf/install_optional.sh
+++ kali/conf/install_optional.sh
@@ -4,4 +4,9 @@ echo "Installing optional tools"
 apt-get install -y man-db w3m exploitdb smbclient dsniff testssl.sh commix hydra vim golang-go mydumper seclists python2
 cd /usr/share/wordlists && git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git
 cd /root/tools && git clone https://github.com/TryCatchHCF/PacketWhisper.git
+cd /root/tools && git clone https://github.com/SneakyBeagle/nwrapy.git
+cd /root/tools/nwrapy/ && ./install.sh
+cd /root/tools && git clone https://github.com/SneakyBeagle/CreepyCrawler.git
+cd /root/tools/CreepyCrawler/ && ./install.sh
+cd /root/tools && git clone https://github.com/trustedsec/egressbuster.git
 echo "Done"
