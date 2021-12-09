4026b125f3937c718f20eb4219d784901e5919d4
jochemste <jochemstevense@hotmail.com>
2021-12-09 15:08:47 +0100

    added packetwhisper

--- kali/conf/install_optional.sh
+++ kali/conf/install_optional.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Installing optional tools"
-apt-get install -y man-db w3m exploitdb smbclient dsniff testssl.sh commix hydra vim golang-go mydumper seclists
+apt-get install -y man-db w3m exploitdb smbclient dsniff testssl.sh commix hydra vim golang-go mydumper seclists python2
 cd /usr/share/wordlists && git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git
+cd /root/tools && git clone https://github.com/TryCatchHCF/PacketWhisper.git
 echo "Done"
