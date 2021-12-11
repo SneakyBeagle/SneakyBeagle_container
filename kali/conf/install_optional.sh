60217ee8e5a44ad0401d900b8a03ac351a637764
jochemste <jochemstevense@hotmail.com>
2021-12-11 16:11:58 +0100

    added sqsh to connect to mssql servers

--- kali/conf/install_optional.sh
+++ kali/conf/install_optional.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing optional tools"
-apt-get install -y man-db w3m exploitdb smbclient dsniff testssl.sh commix hydra vim golang-go mydumper seclists python2
+apt-get install -y man-db w3m exploitdb smbclient dsniff testssl.sh commix hydra vim golang-go mydumper seclists python2 sqsh
 cd /usr/share/wordlists && git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git
 cd /root/tools && git clone https://github.com/TryCatchHCF/PacketWhisper.git
 cd /root/tools && git clone https://github.com/SneakyBeagle/nwrapy.git
