bd46c66130b9eee3b765268f5e7ad05d6a05bfdc
cbk914 <cbk914@riseup.net>
2021-12-16 12:53:32 +0100

    Update

--- redteam/conf/weaponization.sh
+++ redteam/conf/weaponization.sh
@@ -7,5 +7,9 @@ echo "Installing weapon factory"
 cd $WORKDIR
 git clone $GITHUB/WyAtu/CVE-2018-20250.git
 git clone $GITHUB/bhdresh/CVE-2017-8759.git
+git clone $GITHUB/bhdresh/CVE-2017-0199.git
 git clone $GITHUB/rxwx/CVE-2017-8570.git
+git clone $GITHUB/nccgroup/demiguise.git
+git clone $GITHUB/Mr-Un1k0d3r/MaliciousMacroGenerator.git
+git clone $GITHUB/
 echo "Done"
