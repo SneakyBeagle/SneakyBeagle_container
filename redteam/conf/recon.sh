ed9984a89a9083f07611f2a872e461658b4de3ce
cbk914 <cbk914@riseup.net>
2021-12-16 12:29:55 +0100

    Add scripts

--- redteam/conf/recon.sh
+++ redteam/conf/recon.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 WORKDIR="/root/"
+GITHUB="https://github.com"
 
 echo "Installing recon tools"
 cd $WORKDIR
-git clone https://github.com/laramies/theHarvester.git
+git clone $GITHUB/nmap/nmap.git
+git clone $GITHUB/laramies/theHarvester.git
+git clone $GITHUB/laramies/metagoofil.git
+git clone $GITHUB/lanmaster53/recon-ng.git
+git clone $GITHUB/xillwillx/skiptracer.git
+git clone $GITHUB/FortyNorthSecurity/Just-Metadata.git
+git clone $GITHUB/smicallef/spiderfoot.git
+
---
+++ redteam/conf/weaponization.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+WORKDIR="/root/"
+GITHUB="https://github.com"
+
+echo "Installing weapon factory"
+cd $WORKDIR
+git clone $GITHUB/WyAtu/CVE-2018-20250.git
+git clone $GITHUB/bhdresh/CVE-2017-8759.git
+git clone $GITHUB/rxwx/CVE-2017-8570.git
