567ce90e4227013f8cf4b1210774dc6daf6e257d
cbk914 <cbk914@riseup.net>
2021-12-16 13:04:43 +0100

    Update script

--- redteam/conf/weaponization.sh
+++ redteam/conf/weaponization.sh
@@ -5,11 +5,17 @@ GITHUB="https://github.com"
 
 echo "Installing weapon factory"
 cd $WORKDIR
+
 git clone $GITHUB/WyAtu/CVE-2018-20250.git
 git clone $GITHUB/bhdresh/CVE-2017-8759.git
 git clone $GITHUB/bhdresh/CVE-2017-0199.git
 git clone $GITHUB/rxwx/CVE-2017-8570.git
+git clone $GITHUB/koutto/jok3r.git
 git clone $GITHUB/nccgroup/demiguise.git
 git clone $GITHUB/Mr-Un1k0d3r/MaliciousMacroGenerator.git
-git clone $GITHUB/
+git clone $GITHUB/Mr-Un1k0d3r/DKMC.git
+git clone $GITHUB/0xdeadbeefJERKY/Office-DDE-Payloads.git
+git clone $GITHUB/joker25000/DZGEN.git
+git clone $GITHUB/Arno0x/EmbedInHTML.git
+git clone $GITHUB/sevagas/macro_pack.git
 echo "Done"
