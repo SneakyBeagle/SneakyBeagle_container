a6dc57f6733e42ea7b4c31154bb021245a643188
cbk914 <cbk914@riseup.net>
2021-12-16 15:16:31 +0100

    Script updates

--- kali/conf/install_optional.sh
+++ kali/conf/install_optional.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing optional tools"
-apt-get install -y man-db w3m exploitdb smbclient dsniff testssl.sh commix hydra vim golang-go mydumper seclists python2
+apt-get install -y man-db w3m exploitdb smbclient dsniff testssl.sh commix hydra vim golang-go mydumper seclists python2 tar tor curl python3 python3-scapy
 cd /usr/share/wordlists && git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git
 cd /root/tools && git clone https://github.com/TryCatchHCF/PacketWhisper.git
 cd /root/tools && git clone https://github.com/SneakyBeagle/nwrapy.git
@@ -9,4 +9,5 @@ cd /root/tools/nwrapy/ && ./install.sh
 cd /root/tools && git clone https://github.com/SneakyBeagle/CreepyCrawler.git
 cd /root/tools/CreepyCrawler/ && ./install.sh
 cd /root/tools && git clone https://github.com/trustedsec/egressbuster.git
+cd /root/tools && git clone https://github.com/omer-dogan/kali-whoami.git && cd /root/tools/kali-whoami && make install
 echo "Done"
--- redteam/conf/anon.sh
+++ redteam/conf/anon.sh
@@ -6,7 +6,13 @@ GITHUB="https://github.com"
 echo "Installing anonymity tools"
 apt-add-repository ppa:i2p-maintainers/i2p
 apt-get update
-apt-get install i2p
-#apt-get install shadowsocks-libev
-cd $WORKDIR
-git clone $GITHUB/
+apt-get install -y openvpn torsocks proxychains wireguard apt-transport-https lsb-release
+echo "deb [signed-by=/usr/share/keyrings/i2p-archive-keyring.gpg] https://deb.i2p2.de/ $(lsb_release -sc) main" \
+  | sudo tee /etc/apt/sources.list.d/i2p.list
+curl -o i2p-archive-keyring.gpg https://geti2p.net/_static/i2p-archive-keyring.gpg
+cp ~/i2p-archive-keyring.gpg /usr/share/keyrings
+apt install -y i2p i2p-keyring
+#apt-get install shadowsocks-libev 
+#cd $WORKDIR
+#git clone $GITHUB/
+echo "Done"
