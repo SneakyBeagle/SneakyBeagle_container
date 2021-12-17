ec26348aa9babe26b0a10f93265fb31e6e1cd8b2
cbk914 <cbk914@riseup.net>
2021-12-17 13:06:43 +0100

    Added more tools

--- redteam/conf/exfiltration.sh
+++ redteam/conf/exfiltration.sh
@@ -6,4 +6,11 @@ GITHUB="https://github.com"
 echo "Installing exfiltration tools"
 cd $WORKDIR
 git clone $GITHUB/IncideDigital/Mistica.git
+git clone $GITHUB/iagox86/dnscat2.git
+git clone $GITHUB/Arno0x/DNSExfiltrator.git
+git clone $GITHUB/FortyNorthSecurity/Egress-Assess.git
+git clone $GITHUB/PaulSec/DET.git
+git clone $GITHUB/Viralmaniar/Powershell-RAT.git
+git clone $GITHUB/ytisf/PyExfil.git
+git clone $GITHUB/SECFORCE/Tunna.git
 echo "Done"
