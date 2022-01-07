e7f5908d4917c1e5f2fda1ef2cc6f814a8ea9238
cbk914 <cbk914@riseup.net>
2022-01-07 21:32:06 +0100

    Fixed path errors

--- redteam/conf/exfiltration.sh
+++ redteam/conf/exfiltration.sh
@@ -22,6 +22,6 @@ git clone $GITHUB/SECFORCE/Tunna.git
 cd $WORKDIR
 # git clone $GITHUB/py2exe/py2exe.git && cd py2exe && python3 setup.py install
 #cd $WORKDIR
-git clone $GITHUB/ytisf/PyExfil.git && pip3 install -r requirements3.txt
+git clone $GITHUB/ytisf/PyExfil.git && cd PyExfil && pip3 install -r requirements3.txt
 cd $WORKDIR
 echo "Done"
