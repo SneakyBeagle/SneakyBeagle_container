252f50164b44c50c6a498126712f8ee237cc599f
cbk914 <cbk914@riseup.net>
2022-01-07 21:32:06 +0100

    Fixed path errors

--- redteam/conf/privilege_escalation.sh
+++ redteam/conf/privilege_escalation.sh
@@ -12,9 +12,9 @@ git clone $GITHUB/rebootuser/LinEnum.git
 git clone $GITHUB/InteliSecureLabs/Linux_exploit_Suggester.git
 #git clone $GITHUB/sleventyeleven/linuxprivchecker.git
 pip3 install linuxprivchecker
-git clone $GITHUB/diego-treitos/linux-smart-enumeration.git && cd diego-treitos/linux-smart-enumeration.git && chmod 700 lse.sh && cd $WORKDIR
+git clone $GITHUB/diego-treitos/linux-smart-enumeration.git && cd linux-smart-enumeration && chmod 700 lse.sh && cd $WORKDIR
 git clone $GITHUB/411Hall/JAWS.git
-git clone $GITHUB/bitsadmin/wesng.git && cd bitsadmin/wesng.git && python3 wes.py --update && cd $WORKDIR
+git clone $GITHUB/bitsadmin/wesng.git && cd wesng && python3 wes.py --update && cd $WORKDIR
 git clone $GITHUB/absolomb/WindowsEnum.git
 git clone $GITHUB/cckuailong/Log4j_CVE-2021-45046.git
 git clone $GITHUB/lgandx/Responder.git
