18e73743c078539657b205b118e8e56cfa8274af
cbk914 <cbk914@riseup.net>
2022-01-07 17:06:10 +0100

    Add sometools.sh script

--- redteam/conf/evasion.sh
+++ redteam/conf/evasion.sh
@@ -11,4 +11,6 @@ git clone $GITHUB/hfiref0x/UACME.git
 cd $WORKDIR
 git clone $GITHUB/0xsp-SRD/mortar.git
 cd $WORKDIR
+#git clone $GITHUB/oddcod3/Phantom-Evasion.git && cd Phantom-Evasion && chmod +x phantom-evasion.py && python3 phantom-evasion.py --setup
+#cd $WORKDIR
 echo "Done"
