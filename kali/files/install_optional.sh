00b49f19869696889007cfec49daef6cf585aa29
jochemste <jochemstevense@hotmail.com>
2022-01-07 10:19:46 +0100

    updated install_optional script

--- kali/files/install_optional.sh
+++ kali/files/install_optional.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing optional tools"
-apt-get install -y man-db w3m exploitdb smbclient dsniff testssl.sh commix hydra vim golang-go mydumper seclists python2 sqsh
+apt-get install -y man-db w3m exploitdb smbclient dsniff testssl.sh commix hydra vim golang-go mydumper seclists python2 sqsh metasploit-framework
 cd /usr/share/wordlists && git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git
 cd /root/tools && git clone https://github.com/TryCatchHCF/PacketWhisper.git
 cd /root/tools && git clone https://github.com/SneakyBeagle/nwrapy.git
