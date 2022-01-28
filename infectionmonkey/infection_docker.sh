e1f4f50e64471fd466be055194d45768e61cbad0
cbk914 <cbk914@riseup.net>
2022-01-28 15:25:33 +0100

    Update IM docker script

--- infectionmonkey/infection_docker.sh
+++ infectionmonkey/infection_docker.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 # Execute as root
-docker load -i dk.monkeyisland.latest.tar
-docker pull mongo
-mkdir -p /var/monkey-mongo/data/db
-docker run --name monkey-mongo --network=host -v /var/monkey-mongo/data/db:/data/db -d mongo
-docker run --name monkey-island --network=host -d guardicore/monkey-island:1.8.0
+# Only compatible with docker for Linux
+apt-get update && apt-get -y upgrade
+FILE="/usr/bin/wget"
+if [ -f "$FILE" ]; then
+    echo "Let's continue..."
+    else
+    apt-get install wget
+fi
+wget https://guardicore-infectionmonkey.s3.amazonaws.com/Files/1.12.0/InfectionMonkey-docker-v1.12.0.tgz
+tar -xvzf InfectionMonkey-docker-v1.12.0.tgz
+docker pull mongo:4.2
+docker load -i InfectionMonkey-docker-v1.12.0.tar
+# Start MongoDB docker container
+sudo docker run \
+    --name monkey-mongo \
+    --network=host \
+    --volume db:/data/db \
+    --detach \
+    mongo:4.2
+# Start Monkey Island with default self-signed certificate
+sudo docker run \
+    --tty \
+    --interactive \
+    --name monkey-island \
+    --network=host \
+    guardicore/monkey-island:1.12.0
