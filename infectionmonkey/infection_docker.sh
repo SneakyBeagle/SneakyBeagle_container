c81bfb6979aedebc2e03e9f489263a1dd0b014ef
jochemste <jochemstevense@protonmail.com>
2022-02-02 13:47:22 +0100

    fix: Drastically changed the infectionmonkey.sh script. Implemented some failsafes, and made sure it will result in a running monkey island

--- infectionmonkey/infection_docker.sh
+++ infectionmonkey/infection_docker.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+MONKVERS=1.12.0
+MONKNAME=monkey-island
+
+MONGO=mongo:4.2
+MONGONAME=monkey-mongo
+
+INFMONKTGZ=InfectionMonkey-docker-v$MONKVERS.tgz
+INFMONKTAR=InfectionMonkey-docker-v$MONKVERS.tar
+
 # Based on instructions found on https://www.guardicore.com/infectionmonkey/docs/setup/docker/
 
 # Execute as root
@@ -17,24 +26,41 @@ else
 fi
 
 # Download infectionmonkey container
-wget https://guardicore-infectionmonkey.s3.amazonaws.com/Files/1.12.0/InfectionMonkey-docker-v1.12.0.tgz
-# Unpack the archive
-tar -xvzf InfectionMonkey-docker-v1.12.0.tgz
+if [ ! -f ./$INFMONKTGZ ]; then
+    echo "Downloading"
+    wget https://guardicore-infectionmonkey.s3.amazonaws.com/Files/$MONKVERS/$INFMONKTGZ
+    # Unpack the archive
+    echo "Unpacking"
+fi
+if [ ! -f ./$INFMONKTAR ]; then
+    tar -xvzf $INFMONKTGZ
+fi
+
+# Delete Mongo
+(docker stop $MONGONAME; docker rm $MONGONAME) || echo "Mongo does not exist yet"
+
+# Delete Monkey island if it exists
+(docker stop $MONKNAME; docker rm $MONKNAME) || echo "Monkey does not exist yet"
+
 # Pull mongo and load infectionmonkey
-docker pull mongo:4.2
-docker load -i InfectionMonkey-docker-v1.12.0.tar
+echo "Pulling mongo"
+docker pull $MONGO
+echo "Loading infectionmonkey"
+docker load -i $INFMONKTAR
 
 # Start MongoDB docker container
+echo "Starting mongodb"
 docker run \
        --name monkey-mongo \
        --network=host \
        --volume db:/data/db \
        --detach \
-       mongo:4.2
+       $MONGO
 
 # Start Monkey Island with default self-signed certificate (detached)
+echo "Starting Monkey Island"
 docker run \
        --detach \
-       --name monkey-island \
+       --name $MONKNAME \
        --network=host \
-       guardicore/monkey-island:1.12.0
+       guardicore/monkey-island:v$MONKVERS
