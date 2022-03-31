9f949dd9106f702011afd390478cf03664d5cfa9
cbk914 <cbk914@riseup.net>
2022-03-31 17:53:31 +0200

    Add apkleaks

--- redteam/installers/mobile.sh
+++ redteam/installers/mobile.sh
@@ -8,5 +8,6 @@ mkdir $WORKDIR
 echo "Installing mobile tools"
 cd $WORKDIR
 git clone $GITHUB/MobSF/Mobile-Security-Framework-MobSF.git
+git clone $GITHUB/dwisiswant0/apkleaks.git
 cd $WORKDIR
 echo "Done"
